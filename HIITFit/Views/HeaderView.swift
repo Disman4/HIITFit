9c1b0130ce2a2d324a6eba81afd973ff0ef49435
tanaka <tanaka.mawoyo@yahoo.com>
2024-05-29 15:34:08 +0200

    .onClick gesture

--- HIITFit/Views/HeaderView.swift
+++ HIITFit/Views/HeaderView.swift
@@ -13,6 +13,9 @@ struct HeaderView: View {
                 ForEach(Exercise.exercises.indices, id: \.self){ index in
                     let fill = index == selectedTab ? ".fill" : ""
                       Image(systemName: "\(index + 1).circle\(fill)")
+                        .onTapGesture {
+                            selectedTab = index
+                        }
                 }
             }
             .font(.title2)
